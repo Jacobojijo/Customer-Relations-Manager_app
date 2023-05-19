ead2dbabf6d3de6412d80efd595dd4fe07158905
Jacobojijo <jacobojio2017@gmail.com>
2023-05-19 15:13:55 +0300

    host

--- crm/settings.py
+++ crm/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'NAME': 'ojijoco',
         'USER': 'root',
         'PASSWORD': 'Jack1998',
-        'HOST': 'localhost',
+        'HOST': '127.0.0.1',
         'PORT': '3306'
     }
 }
